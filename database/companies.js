6b38c23cb3628164ede76441934546819766c0b7
Carlos-vargs <cvargaslopez769@gmail.com>
2023-01-17 14:22:51 -0600

    ci: add data for db

---
+++ database/companies.js
@@ -0,0 +1,164 @@
+const data = [
+  {
+    id: "1",
+    name: "Odit voluptatum tenetur iusto",
+    description:
+      "Ex quisquam neque eos et eveniet. Rem voluptatem voluptatibus nam. Nihil tenetur sed quidem.",
+    productCount: 4,
+    ownerId: "1",
+  },
+  {
+    id: "2",
+    name: "Voluptas sunt quo doloremque",
+    description:
+      "Sunt laborum quibusdam vel maxime ut voluptas ipsam. Optio eligendi qui et maiores expedita. Repellendus ipsam ut impedit voluptatibus porro cum consequatur. Voluptates fugit distinctio ducimus odit officiis.",
+    productCount: 4,
+    ownerId: "1",
+  },
+  {
+    id: "3",
+    name: "Commodi soluta consequuntur alias",
+    description:
+      "Eos nihil nihil voluptatem voluptatem. Distinctio nihil voluptas vitae quisquam amet. Est soluta inventore consequatur distinctio exercitationem est.",
+    productCount: 4,
+    ownerId: "2",
+  },
+  {
+    id: "4",
+    name: "Quasi reiciendis autem culpa",
+    description:
+      "Eum repellendus aspernatur et nulla in eligendi. In et eveniet aperiam voluptate cum aliquid et repellendus. Est nihil exercitationem repellat enim. Quia dolorem tempora qui praesentium.",
+    productCount: 4,
+    ownerId: "2",
+  },
+  {
+    id: "5",
+    name: "Provident omnis aut voluptate",
+    description:
+      "Asperiores sit aut mollitia mollitia libero earum numquam. Beatae minus nisi ipsam eum. Tenetur eius repellendus et numquam. Sed dolores blanditiis quaerat dolorum. Officia et neque et.",
+    productCount: 4,
+    ownerId: "3",
+  },
+  {
+    id: "6",
+    name: "Quas ut aliquam voluptatem",
+    description:
+      "Est voluptatem ex voluptatibus occaecati sit aut dolor rerum. Dolor nisi iste dolorum fugiat.",
+    productCount: 4,
+    ownerId: "3",
+  },
+  {
+    id: "7",
+    name: "Aut aut minus sed",
+    description:
+      "Iure est totam nostrum facere quis pariatur. Minima dolores veritatis unde et modi. Quos rerum possimus tempora itaque eligendi ipsam error. Omnis voluptatem placeat fugit consectetur recusandae et deleniti. Nisi voluptate est delectus quasi ab est voluptatem eos.",
+    productCount: 4,
+    ownerId: "4",
+  },
+  {
+    id: "8",
+    name: "Qui quibusdam voluptas id",
+    description:
+      "Consequatur est distinctio consectetur quisquam id. Et ut facilis aut. Quae alias nulla provident eum non neque non.",
+    productCount: 4,
+    ownerId: "4",
+  },
+  {
+    id: "9",
+    name: "Atque nemo rerum aut",
+    description:
+      "Reprehenderit dolores aut minus veniam officiis. Cumque ab laboriosam aut accusamus non sunt. Sint officia sint illo quo adipisci rerum qui.",
+    productCount: 4,
+    ownerId: "5",
+  },
+  {
+    id: "10",
+    name: "Eligendi aut unde ducimus",
+    description:
+      "Voluptate sapiente quo quo in ea iusto. Qui sint fugiat ut et velit. Quae sunt facilis adipisci iste. Provident nam culpa natus et accusantium autem voluptas ut.",
+    productCount: 4,
+    ownerId: "5",
+  },
+  {
+    id: "11",
+    name: "Iure enim deserunt iusto",
+    description:
+      "Illum voluptatum sunt neque vel. Doloribus adipisci et non. Accusantium voluptas debitis necessitatibus quis.",
+    productCount: 4,
+    ownerId: "6",
+  },
+  {
+    id: "12",
+    name: "Corporis quae aut est",
+    description:
+      "Voluptatem aliquam odio distinctio inventore. Aut blanditiis consequatur rerum molestiae ipsum. Cum autem ullam culpa enim molestias quae dolorem consequatur.",
+    productCount: 4,
+    ownerId: "6",
+  },
+  {
+    id: "13",
+    name: "Nostrum dolore officiis voluptatem",
+    description:
+      "Sint voluptatem quia itaque illum excepturi eos earum laboriosam. Accusantium voluptas aut sit ex non pariatur voluptatum. Rerum et voluptatem eum et enim a. Distinctio et quasi fugit.",
+    productCount: 4,
+    ownerId: "7",
+  },
+  {
+    id: "14",
+    name: "Omnis itaque quod eius",
+    description:
+      "Officia iusto aut est eos. Pariatur placeat et et sunt quaerat provident omnis. Eos corrupti nihil ut aut.",
+    productCount: 4,
+    ownerId: "7",
+  },
+  {
+    id: "15",
+    name: "Et aut quidem consequuntur",
+    description:
+      "Quidem est est molestiae laboriosam maiores natus voluptatibus maiores. Et ut veniam voluptatibus ab. Debitis quia ipsam dolorem et.",
+    productCount: 4,
+    ownerId: "8",
+  },
+  {
+    id: "16",
+    name: "Fugiat iste reprehenderit quia",
+    description:
+      "Magnam animi facilis quia praesentium consequatur sit inventore. Libero adipisci fugit quia et rerum. Necessitatibus fugit repellendus ducimus rerum ipsum voluptas.",
+    productCount: 4,
+    ownerId: "8",
+  },
+  {
+    id: "17",
+    name: "Atque maxime occaecati enim",
+    description:
+      "Iusto rem ipsum fugit est earum reprehenderit quia incidunt. Cumque explicabo qui explicabo voluptas autem explicabo laudantium. Perspiciatis nihil vitae dolor vero est non dolorem.",
+    productCount: 4,
+    ownerId: "9",
+  },
+  {
+    id: "18",
+    name: "Ut distinctio magnam enim",
+    description:
+      "Iure distinctio beatae consequuntur dolorem ut. Eaque voluptate quam eaque consequatur et. Quia minus perspiciatis pariatur. Doloribus esse at repellat qui et et.",
+    productCount: 4,
+    ownerId: "9",
+  },
+  {
+    id: "19",
+    name: "Molestiae quia minus illo",
+    description:
+      "Accusantium et delectus ab adipisci harum. Ratione vero quo nam deleniti magnam voluptatum. Est vero dicta quod debitis culpa quia sequi. Consequatur magni impedit mollitia. Consectetur est alias eum consequatur tempore numquam.",
+    productCount: 4,
+    ownerId: "10",
+  },
+  {
+    id: "20",
+    name: "Non consequuntur quas porro",
+    description:
+      "Consequatur animi repudiandae a quis iste perferendis. Quasi eveniet ex laudantium aut dolores veritatis. Est sunt id laudantium possimus reiciendis deleniti recusandae. Molestias repellat sequi velit.",
+    productCount: 4,
+    ownerId: "10",
+  },
+];
+
+export default data;
---
+++ database/products.js
@@ -0,0 +1,42 @@
+import users from "./users";
+
+const data = [
+  {
+    id: "1",
+    name: "Rerum error iure adipisci",
+    description:
+      "Quo accusamus perferendis repellendus dolores occaecati voluptatem. Neque quas eos molestiae ut pariatur commodi facere ab. Nisi quia ab sed maiores. Iusto ut qui totam fugiat. Quis velit quos commodi autem est.",
+    avg: 3.4,
+    hasRated: null,
+    usersRated: users,
+  },
+  {
+    id: "2",
+    name: "Excepturi facilis assumenda ex",
+    description:
+      "Consequatur nemo impedit quaerat perferendis. Eligendi omnis eveniet beatae fugit exercitationem dolor sunt. Sint nihil at aspernatur quo labore nihil.",
+    avg: 3.5,
+    hasRated: null,
+    usersRated: users,
+  },
+  {
+    id: "3",
+    name: "Ex ab voluptatem optio",
+    description:
+      "Et ad fuga molestiae. Nobis modi ut natus doloribus quasi omnis veniam. Minima cumque totam sed alias est omnis est libero. Voluptatum sunt animi quaerat.",
+    avg: 3.5,
+    hasRated: null,
+    usersRated: users,
+  },
+  {
+    id: "4",
+    name: "Ab quam illum vitae",
+    description:
+      "Repudiandae assumenda at ullam nulla dolorem. Accusantium sit ut sapiente nulla rerum et facere reiciendis. Quae dolores vel aut vel.",
+    avg: 4.3,
+    hasRated: null,
+    usersRated: users,
+  },
+];
+
+export default data;
